49a302828cca692c15084d78902c200977251368
gauravmittal54 <61792468+gauravmittal54@users.noreply.github.com>
2023-10-22 02:44:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const optionRoutes = require('./routes/optionRoutes');
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerJSDoc = require('swagger-jsdoc');
+const cors = require('cors');
+
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 8000;
 const serverUrl = process.env.SERVER_URL || `http://localhost:${port}`;
 
 app.use(express.json());
+
 
 const options = {
     definition: {
