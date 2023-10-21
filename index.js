431197b86edb7e19325fd7349f31cc0174c61cc0
gauravmittal54 <61792468+gauravmittal54@users.noreply.github.com>
2023-10-22 02:41:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,69 @@ const questionRoutes = require('./routes/questionRoutes');
 const optionRoutes = require('./routes/optionRoutes');
 
 const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./swaggerUi/swaggerConfig'); 
+const swaggerJSDoc = require('swagger-jsdoc');
 
 const app = express();
 const port = process.env.PORT || 8000;
+const serverUrl = process.env.SERVER_URL || `http://localhost:${port}`;
 
 app.use(express.json());
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Polling-Api',
+            version: '1.0.0'
+        },
+        servers: [
+            {
+                url: serverUrl,
+            },
+        ],
+        components: {
+            schemas: {
+                Option: {
+                    type: 'object',
+                    properties: {
+                        text: {
+                            type: 'string'
+                        },
+                        votes: {
+                            type: 'integer'
+                        },
+                        link_to_vote: {
+                            type: 'string'
+                        },
+                        question_id: {
+                            type: 'string',
+                            format: 'uuid'
+                        }
+                    },
+                    required: ['text', 'votes', 'question_id']
+                },
+                Question: {
+                    type: 'object',
+                    properties: {
+                        title: {
+                            type: 'string'
+                        },
+                        options: {
+                            type: 'array',
+                            items: {
+                                $ref: '#/components/schemas/Option'
+                            }
+                        }
+                    },
+                    required: ['title']
+                }
+            }
+        }
+    },
+    apis: ['./routes/*']
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 // Use the routes
 app.use('/questions', questionRoutes);
@@ -19,5 +76,7 @@ app.use('/options', optionRoutes);
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(port, (err) => {
-    console.log(`localhost running at port: ${port}`);
+    console.log(`server running at : ${serverUrl}`);
 });
+
+
