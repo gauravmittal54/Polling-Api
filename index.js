29ee28edcdb882579f6040269fdeedd03bc4fe0b
gauravmittal54 <61792468+gauravmittal54@users.noreply.github.com>
2023-10-22 01:21:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const express = require("express");
+const express = require('express');
 require('./connection/db_connection');
 const questionRoutes = require('./routes/questionRoutes');
 const optionRoutes = require('./routes/optionRoutes');
-const routes = require('./routes/optionRoutes');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swaggerUi/swaggerConfig'); 
 
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(routes)
 
 // Use the routes
 app.use('/questions', questionRoutes);
 app.use('/options', optionRoutes);
 
+// Set up Swagger UI
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.listen(port,(err)=>{
-    console.log(`localhost running at port : ${port}`);
+app.listen(port, (err) => {
+    console.log(`localhost running at port: ${port}`);
 });
