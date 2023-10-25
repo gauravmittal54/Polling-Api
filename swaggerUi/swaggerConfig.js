11f957592d93c39f662cd3ef6f4e0238e6a55549
gauravmittal54 <61792468+gauravmittal54@users.noreply.github.com>
2023-10-25 20:28:33 +0530

    Update swaggerConfig.js

--- swaggerUi/swaggerConfig.js
+++ swaggerUi/swaggerConfig.js
@@ -6,8 +6,6 @@ const options = {
         info: {
             title: 'Polling-Api',
             description: 'This is an API for managing polls for each option created for a corresponding question. Here are the steps:\n\n1. **Step 1:** Create a question using the "Create Question" API.\n2. **Step 2:** Add an option to the question using the question ID generated.\n3. **Step 3:** Add votes to that option using the "Add Vote to Option" API.\n\nCreated by Gaurav. [GitHub Repository](https://github.com/gauravmittal54/Polling-Api/tree/main)',        },
-            version: '1.0.0'
-        },
         servers: [
             {
                 url: global.serverUrl
